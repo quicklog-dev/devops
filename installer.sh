942b8d6ad7368252c63651b845a1bb35480d05cb
poonai <rbalajis25@gmail.com>
2021-07-28 00:22:01 +0530

    first commit

---
+++ installer.sh
@@ -0,0 +1,5 @@
+echo "Installing quicklog 🚀🚀"
+wget https://quicklog.sgp1.digitaloceanspaces.com/quicklog
+echo "Enter password to move the binary to the bin folder"
+sudo mv quicklog /usr/local/bin
+echo "quicklog installed 🦸"
